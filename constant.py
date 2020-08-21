224e9a445a67e0dee4f22cd5532620fcfc58c1d0
Wook-2 <bwson98@gmail.com>
2020-08-21 12:16:40 +0900

    bug fix

--- constant.py
+++ constant.py
@@ -1,3 +1,3 @@
 
-ocr_url = 'https://master-easy-ocr-wook-2.endpoint.ainize.ai/'
+ocr_url = 'https://master-easy-ocr-wook-2.endpoint.ainize.ai/word_extraction'
 tts_url = 'https://master-wave-rnn-woomurf.endpoint.ainize.ai/tts'
